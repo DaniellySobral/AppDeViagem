fa815ef9d1b1e04e1a5367ddaf8e249983dd4677
danielly <daniellysobral@outlook.com>
2024-10-06 13:34:48 -0300

    Resolvi o bug das imagens retirando algumas delas pois estava consumindo muita memoria, devido a isso travava o app. Adicionei também o link de consulta do preço do dollar na pagina de calcular dolar, fiz isso aplicando o ClickableSpan no texto via código kotlin.

--- app/src/main/java/com/example/testetravelapp/MainActivity.kt
+++ app/src/main/java/com/example/testetravelapp/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.testetravelapp
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.buildSpannedString
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -23,8 +32,6 @@ class MainActivity : AppCompatActivity() {
         R.drawable.italia,
         R.drawable.japan,
         R.drawable.balao,
-        R.drawable.lugares,
-        R.drawable.montanha,
         R.drawable.notredame
 
     )
@@ -33,15 +40,35 @@ class MainActivity : AppCompatActivity() {
     lateinit var botaoAnotacao: Button
     lateinit var converterMoeda: ImageView
     lateinit var calcularCombustivel: ImageView
+    private lateinit var rodarLoop: Runnable
+    private lateinit var lidarAnimacao: Handler
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val textoLink = findViewById<TextView>(R.id.linkTexto)
+
+        val textoCompleto = textoLink.text.toString()
+        val colocandoLink = SpannableString(textoCompleto)
+
+        val textoClicavel = object : ClickableSpan(){
+            override fun onClick(widget: View) {
+               val executaIntent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.ipeadata.gov.br/ExibeSerie.aspx?stub=1&serid=38590&module=M"))
+                startActivity(executaIntent)
+            }
+        }
+
+        colocandoLink.setSpan(textoClicavel, 20, 41, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        textoLink.text = colocandoLink
+        textoLink.movementMethod = LinkMovementMethod.getInstance()
+
         val caixaImagens = findViewById<ImageView>(R.id.caixaBorda)
-        val lidarAnimacao = Handler(Looper.getMainLooper())
-        val rodarLoop = object : Runnable{
+        lidarAnimacao = Handler(Looper.getMainLooper())
+         rodarLoop = object : Runnable{
             override fun run(){
                 val fadeOut = ObjectAnimator.ofFloat(caixaImagens, "alpha", 1f, 0f)
                 fadeOut.duration = 1000
@@ -55,11 +82,18 @@ class MainActivity : AppCompatActivity() {
                         val fadeIn = ObjectAnimator.ofFloat(caixaImagens, "alpha", 0f, 1f)
                         fadeIn.duration = 1500
                         fadeIn.start()
+
+                        fadeIn.addListener(object : AnimatorListenerAdapter() {
+                            override fun onAnimationEnd(animation: Animator){
+
+                                lidarAnimacao.postDelayed(rodarLoop, 4500)
+                            }
+
+                        })
                     }
                 })
                 fadeOut.start()
 
-               lidarAnimacao.postDelayed(this,4500)
             }
         }
         lidarAnimacao.post(rodarLoop)
@@ -93,5 +127,9 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        lidarAnimacao.removeCallbacks(rodarLoop) // Remove o callback para evitar chamadas após a destruição
     }
 }
